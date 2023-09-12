4d12ed9043ed82995f913945b4b174f993492804
GustavoHMarchi <gustavo.holiveira@sptech.school>
2023-09-12 12:27:13 -0300

    Alterações no site

--- python/captacaoSimples.py
+++ python/captacaoSimples.py
@@ -1,22 +1,22 @@
-from mysql.connector import connect
+#from mysql.connector import connect
 import psutil
 import platform
 import time
 
-def mysql_connection(host, user, passwd, database=None):
-    connection = connect(
-        host=host,
-        user=user,
-        passwd=passwd,
-        database=database
-    )
-    return connection
-connection = mysql_connection('localhost', 'pedro', 'pedro0610', 'MedConnect')
+#def mysql_connection(host, user, passwd, database=None):
+ #   connection = connect(
+  #      host=host,
+   #     user=user,
+    #    passwd=passwd,
+     #   database=database
+    #)
+    #return connection
+#connection = mysql_connection('localhost', 'pedro', 'pedro0610', 'MedConnect')
 
 while True:
     memoria = psutil.virtual_memory()[2]
     cpu = psutil.cpu_percent(None)
-    print('Porcentagem processaodr:',cpu,'Porcentagem Memoria RAM:',  memoria,)
+    print('Porcentagem processador:',cpu,'Porcentagem Memoria RAM:',  memoria,)
     
 
 
