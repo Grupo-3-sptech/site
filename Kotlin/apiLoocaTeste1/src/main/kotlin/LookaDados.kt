709af9682349be5b6558e3e93c84f4f33a8bd014
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-10-06 15:06:16 -0300

    feat nova lógica de classes do monitoramento e insert funcional

--- Kotlin/apiLoocaTeste1/src/main/kotlin/LookaDados.kt
+++ Kotlin/apiLoocaTeste1/src/main/kotlin/LookaDados.kt
@@ -2,15 +2,47 @@ import com.github.britooo.looca.api.core.Looca
 import com.github.britooo.looca.api.group.dispositivos.DispositivoUsb
 import org.apache.commons.dbcp2.BasicDataSource
 import org.springframework.jdbc.core.JdbcTemplate
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 class LookaDados {
     val looca = Looca()
+    val bdInter: JdbcTemplate
+    val processador = looca.processador
+    var id = processador.id
+
+    init {
+
+        val dataSource = BasicDataSource()
+        dataSource.driverClassName = "com.mysql.cj.jdbc.Driver"
+        val serverName = "localhost"
+        val mydatabase = "medconnect"
+        dataSource.username = "root"
+        dataSource.password = "enzo123"
+        dataSource.url = "jdbc:mysql://$serverName/$mydatabase"
+        bdInter = JdbcTemplate(dataSource)
+    }
+
+    fun all() {
+        sistema()
+        memoria()
+        processador()
+        grupoDeDiscos()
+        grupoDeServicos()
+        grupoDeProcessos()
+        Dispositivo()
+        rede()
+        janelas()
+    }
 
 
     fun sistema() {
         val sistema = looca.sistema
 
+        println(sistema)
+
         var fabricante = sistema.fabricante
+
 
         var incializando = sistema.inicializado
 
@@ -22,27 +54,33 @@ class LookaDados {
 
         var tempDeAtividade = sistema.tempoDeAtividade
 
+
     }
 
-    fun memoria(){
+    fun memoria() {
         val memoria = looca.memoria
+
         var emUso = memoria.emUso
+
         var disponivel = memoria.disponivel
+
         var total = memoria.total
     }
-    fun processador(){
-        val processador = looca.processador
+
+    fun processador() {
 
         var fabricante = processador.fabricante
 
-        var id = processador.id
 
         var frequencia = processador.frequencia
 
         var nome = processador.nome
 
+
         var identificador = processador.identificador
-        //println(identificador)
+        println("ids abaixo")
+        println(id)
+        println("ids acima")
 
         var microarquitetura = processador.microarquitetura
 
@@ -53,13 +91,25 @@ class LookaDados {
         var uso = processador.uso
 
         var numPacotFisico = processador.numeroPacotesFisicos
+
+        bdInter.execute(
+            """
+       
+       INSERT INTO Registros (fkRoboRegistro, HorarioDado, dado, fkComponente) 
+VALUES 
+  (1, '${LocalDateTime.now()}', ${uso}, 1);
+       
+         """
+        )
     }
-    fun grupoDeDiscos(){
+
+    fun grupoDeDiscos() {
         val grupoDeDiscos = looca.grupoDeDiscos
+
+
         var qtdDeDisco = grupoDeDiscos.quantidadeDeDiscos
         //I live in the Rua hadock lobo Building on West 595 Street on the 2nd floor. My name is Enzo I’m 18 years old. There is an idea of a Enzo. Some kind of abstraction. But there is no real me.
 
-        //não entendi direito essa variavel fica muito grande não recomendo usar por enquanto
         var discos = grupoDeDiscos.discos
 
         var volumes = grupoDeDiscos.volumes
@@ -67,12 +117,18 @@ class LookaDados {
         var tamanhoTotal = grupoDeDiscos.tamanhoTotal
 
         var qtdVolumes = grupoDeDiscos.quantidadeDeVolumes
+
+        var nome = discos[0].nome
+
+        var serial = discos[0].serial
     }
 
-    fun grupoDeServicos(){
+    fun grupoDeServicos() {
         val grupoDeServicos = looca.grupoDeServicos
-
         var servicos = grupoDeServicos.servicos
+        var nome = servicos[0].nome
+        var estado = servicos[0].estado
+        var pid = servicos[0].pid
         var servicosAtivos = grupoDeServicos.servicosAtivos
         var sevicosInativos = grupoDeServicos.servicosInativos
         var totalDeServiços = grupoDeServicos.totalDeServicos
@@ -80,38 +136,35 @@ class LookaDados {
         var totalServicosInativos = grupoDeServicos.totalServicosInativos
     }
 
-    fun grupoDeProcessos(){
+    fun grupoDeProcessos() {
         val grupoDeProcessos = looca.grupoDeProcessos
         var processos = grupoDeProcessos.processos
-        var totalProcessos =grupoDeProcessos.totalProcessos
-        var totalThreads =grupoDeProcessos.totalThreads
+        var totalProcessos = grupoDeProcessos.totalProcessos
+        var totalThreads = grupoDeProcessos.totalThreads
     }
 
 
-    fun Dispositivo(){
+    fun Dispositivo() {
         val DispositivoUsbGp = looca.dispositivosUsbGrupo
         var totalConectados = DispositivoUsbGp.totalDispositvosUsbConectados
         var dispositivosUsb = DispositivoUsbGp.dispositivosUsb
-        // println(dispositivosUsb)
-        //println(dispositivosUsb[0].nome)
         var dispositivosUsbConectados = DispositivoUsbGp.dispositivosUsbConectados
         var totalDispositvosUsb = DispositivoUsbGp.totalDispositvosUsb
     }
 
-    fun rede(){
+    fun rede() {
         val rede = looca.rede
+        println(rede)
         var parametros = rede.parametros
         var grupoDeInterfaces = rede.grupoDeInterfaces
     }
-    fun janelas(){
+
+    fun janelas() {
         val janela = looca.grupoDeJanelas
         var janelas = janela.janelas
         var janelasVisiveis = janela.janelasVisiveis
         var totalJanelas = janela.totalJanelas
     }
-
-
-
 
 
 }
--- Kotlin/apiLoocaTeste1/src/main/kotlin/looca.kt
+++ Kotlin/apiLoocaTeste1/src/main/kotlin/looca.kt
@@ -1,34 +1,4 @@
-import com.github.britooo.looca.api.core.Looca
-import org.apache.commons.dbcp2.BasicDataSource
-import org.springframework.jdbc.core.JdbcTemplate
-
-
 fun main() {
-    val dataSource = BasicDataSource()
-    dataSource.driverClassName = "com.mysql.cj.jdbc.Driver"
-    val serverName = "localhost"
-    val mydatabase = "medconnect"
-    dataSource.username = "root"
-    //não esqueca de mudar aqui em baixo
-    dataSource.password = "enzo123"
-    dataSource.url = "jdbc:mysql://$serverName/$mydatabase"
-    val bdInter = JdbcTemplate(dataSource)
-
-
-
-    var looka1 = LookaDados()
-    looka1.sistema()
-    looka1.memoria()
-    looka1.processador()
-    looka1.grupoDeDiscos()
-    looka1.grupoDeServicos()
-    looka1.grupoDeProcessos()
-    looka1.Dispositivo()
-    looka1.rede()
-    looka1.janelas()
-
-    //bdInter.execute("""
-        //insert into statusRobo(nome) value
-        //('${fabricante}')
-      //  """)
+    val looka1 = LookaDados()
+    looka1.all()
 }
