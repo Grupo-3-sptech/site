7038b670af51aa6f883eb592d70771af426106b3
GabrielBoos22 <gabriel.bduarte@sptech.school>
2023-10-22 04:10:54 -0300

    Dashboard com resumo e novos dados
    Resumo da CPU, MEMORIA, DISCO, REDE funcionando os dados, podendo escolher o tempo. Se for diferente do atual, exibe as médias.

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "medconnect",
-    user: "admin",
-    password: "admin",
+    user: "root",
+    password: "",
 };
 
 function executar(instrucao) {
