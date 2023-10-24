c7166c8d387e772b996cb8c75b3544b7edc3576c
Kayky <kayky.pedroso@sptech.school>
2023-10-24 00:51:35 -0300

    feat: Relatório funcional com exibição dinâmica

--- site/src/models/alertasModel.js
+++ site/src/models/alertasModel.js
@@ -17,21 +17,36 @@ function buscarUltimosAlertas() {
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `
-        SELECT * FROM Alerta WHERE 
-        tipo_alerta = "critico"
-        AND dtHora >= date_sub(now(), INTERVAL 1 MINUTE);
+        SELECT
+            *,
+        DATE_FORMAT(dtHora, '%d/%m/%Y %H:%i:%s') AS data_formatada
+        FROM Alerta
+        WHERE
+        tipo_alerta = 'critico'
+        AND dtHora >= DATE_SUB(NOW(), INTERVAL 1 MINUTE);
+
         `;
 
         instrucaoSql2 = `
-        SELECT * FROM Alerta WHERE 
-        tipo_alerta = "urgente"
-        AND dtHora >= date_sub(now(), INTERVAL 1 MINUTE);
+        SELECT
+            *,
+        DATE_FORMAT(dtHora, '%d/%m/%Y %H:%i:%s') AS data_formatada
+        FROM Alerta
+        WHERE
+        tipo_alerta = 'urgente'
+        AND dtHora >= DATE_SUB(NOW(), INTERVAL 1 MINUTE);
+
         `;
 
         instrucaoSql3 = `
-        SELECT * FROM Alerta WHERE 
-        tipo_alerta = "alerta"
-        AND dtHora >= date_sub(now(), INTERVAL 1 MINUTE);
+        SELECT
+            *,
+        DATE_FORMAT(dtHora, '%d/%m/%Y %H:%i:%s') AS data_formatada
+        FROM Alerta
+        WHERE
+        tipo_alerta = 'alerta'
+        AND dtHora >= DATE_SUB(NOW(), INTERVAL 1 MINUTE);
+
         `;
 
     } else {
@@ -42,18 +57,18 @@ function buscarUltimosAlertas() {
     console.log("Executando a instrução SQL: \n" + instrucaoSql + instrucaoSql2 + instrucaoSql3);
 
 
-    return executarQueryEDevolverObjetoJSON(instrucaoSql,instrucaoSql2, instrucaoSql3)
+    return executarQueryEDevolverObjetoJSON(instrucaoSql, instrucaoSql2, instrucaoSql3)
 
 }
 
-function buscarQuantidadeDeAlertas(intervalo){
+function buscarQuantidadeDeAlertas(intervalo) {
 
     var instrucaoSql = ''
     var instrucaoSql2 = ''
     var instrucaoSql3 = ''
-    
-        if (process.env.AMBIENTE_PROCESSO == "producao") {
-            instrucaoSql = `select top ${limite_linhas}
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top ${limite_linhas}
             dht11_temperatura as temperatura, 
             dht11_umidade as umidade,  
                             momento,
@@ -61,35 +76,35 @@ function buscarQuantidadeDeAlertas(intervalo){
                         from medida
                         where fk_aquario = ${idAquario}
                         order by id desc`;
-        } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-            instrucaoSql = `
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `
             SELECT count(idAlerta) as alertas FROM Alerta WHERE 
             tipo_alerta = "critico"
             AND dtHora >=  date_sub(now(), INTERVAL 1 ${intervalo});
 
             `;
-    
-            instrucaoSql2 = `
+
+        instrucaoSql2 = `
             SELECT count(idAlerta) as alertas FROM Alerta WHERE 
             tipo_alerta = "urgente"
             AND dtHora >=  date_sub(now(), INTERVAL 1 ${intervalo});
             `;
-    
-            instrucaoSql3 = `
+
+        instrucaoSql3 = `
             SELECT count(idAlerta) as alertas FROM Alerta WHERE 
             tipo_alerta = "alerta"
             AND dtHora >=  date_sub(now(), INTERVAL 1 ${intervalo});
             `;
-    
-        } else {
-            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-            return
-        }
 
-        return executarQueryEDevolverObjetoJSON(instrucaoSql,instrucaoSql2,instrucaoSql3)
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    return executarQueryEDevolverObjetoJSON(instrucaoSql, instrucaoSql2, instrucaoSql3)
 }
 
-async function executarQueryEDevolverObjetoJSON(instrucaoSql, instrucaoSql2, instrucaoSql3){
+async function executarQueryEDevolverObjetoJSON(instrucaoSql, instrucaoSql2, instrucaoSql3) {
 
     const atencao = await database.executar(instrucaoSql)
     const urgente = await database.executar(instrucaoSql2)
