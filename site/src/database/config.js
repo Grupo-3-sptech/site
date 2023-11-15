ce9c13fcd1243584481e7a21b0f370a2c25ee29d
Kayky <kayky.pedroso@sptech.school>
2023-11-15 00:02:07 -0300

    feat: criando estrutura html e primeira estilização

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "medconnect",
-    user: "root",
-    password: "5505",
+    user: "medconnect",
+    password: "medconnect123",
 };
 
 function executar(instrucao) {
